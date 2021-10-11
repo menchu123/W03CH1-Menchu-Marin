6050387d98b69f04256e100a9db17e04b959d6fc
Menchu MV <mcmvillalobos@gmail.com>
2021-10-11 22:09:07 +0200

    Failing at adding character overlay list

--- public/index.js
+++ public/index.js
@@ -33,13 +33,58 @@ const addCharacterPhoto = (img) => {
 };
 
 const addCharacterName = (personaje) => {
-  const characterName = document.querySelector(".character__age");
+  const characterName = document.querySelector(".character__name");
   characterName.textContent = `${personaje.nombre} ${personaje.familia}`;
 };
 
 const addCharacterAge = (personaje) => {
   const characterAge = document.querySelector(".character__age");
   characterAge.textContent = `Edad: ${personaje.edad}`;
+};
+
+const isCharacterAlive = (personaje) => {
+  const alive = document.querySelector(".fa-thumbs-up");
+  const dead = document.querySelector(".fa-thumbs-down");
+
+  if (personaje.vivo) {
+    dead.style.display = "none";
+  } else {
+    alive.style.display = "none";
+  }
+};
+
+// const whatProperties = (personaje) => {
+//   let properties = [];
+//   if (personaje instanceof Rey) {
+
+//   }
+// }
+console.log(bronn.luchador);
+
+const addCharacterData = (personaje) => {
+  const dataList = document.querySelector(".charactar__dataList");
+  const dataElement = document.createElement("li");
+  if (personaje instanceof Rey) {
+    dataList.appendChild(dataElement);
+    dataElement.innerHTML = "";
+    dataElement.textContent = `Años de reinado: ${personaje.anosReinado}`;
+  } else if (personaje instanceof Luchador) {
+    dataList.appendChild(dataElement);
+    dataElement.innerHTML = "";
+    dataElement.textContent = `Arma: ${personaje.arma}`;
+    const dataElement2 = dataElement.cloneNode(true);
+    dataElement2.textContent = `Destreza: ${personaje.destreza}`;
+  } else if (personaje instanceof Escudero) {
+    dataList.appendChild(dataElement);
+    dataElement.innerHTML = "";
+    dataElement.textContent = `Sirve a: ${personaje.luchador.nombre}`;
+    const dataElement2 = dataElement.cloneNode(true);
+    dataElement2.textContent = `Peloteo: ${personaje.pelotismo}`;
+  } else if (personaje instanceof Asesor) {
+    dataList.appendChild(dataElement);
+    dataElement.innerHTML = "";
+    dataElement.textContent = `Asesora a: ${personaje.asesorado.nombre}`;
+  }
 };
 
 const characterCardAdd = (array) => {
@@ -50,6 +95,8 @@ const characterCardAdd = (array) => {
     addCharacterPhoto(`img/${character.nombre}.jpg`);
     addCharacterName(character);
     addCharacterAge(character);
+    isCharacterAlive(character);
+    addCharacterData(character);
   }
 };
 
