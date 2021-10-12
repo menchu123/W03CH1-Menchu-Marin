4e0ff714cf6403f98361199460a5f474119a80f9
Menchu MV <mcmvillalobos@gmail.com>
2021-10-12 18:29:12 +0200

    First Component test complete and passed

--- public/scripts/Component.test.js
+++ public/scripts/Component.test.js
@@ -2,7 +2,16 @@ import Component from "./Component.js";
 
 describe("Given a Component class", () => {
   describe("When creating a new object and giving it a section tag parent, a class 'potato' and a 'p' tag", () => {
-    test("Then it creates a", () => {});
+    test("Then it creates a 'p' element with a class 'potato' inside a section tag", () => {
+      const section = document.createElement("section");
+      const className = "potato";
+      const tag = "p";
+
+      const newComponent = new Component(section, "potato", "p");
+      const isTherePotato = section.querySelector("p.potato");
+
+      expect(isTherePotato).not.toBeNull();
+    });
   });
 });
 
