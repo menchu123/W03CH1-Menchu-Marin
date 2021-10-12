68b992456c2b9ce269bc2d0eebd18f21354042ef
Menchu MV <mcmvillalobos@gmail.com>
2021-10-12 18:21:51 +0200

    First test structured

---
+++ public/scripts/Component.test.js
@@ -0,0 +1,9 @@
+import Component from "./Component.js";
+
+describe("Given a Component class", () => {
+  describe("When creating a new object and giving it a section tag parent, a class 'potato' and a 'p' tag", () => {
+    test("Then it creates a", () => {});
+  });
+});
+
+export default Component;
