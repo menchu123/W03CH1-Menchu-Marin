b6a5da74b235c955a8aada22c047326d9f3f49fc
Menchu MV <mcmvillalobos@gmail.com>
2021-10-12 17:43:55 +0200

    Removed extra code in index.js

--- public/index.js
+++ public/index.js
@@ -23,17 +23,3 @@ const tyrion = new Asesor("Tyrion", Personaje.familia.lannister, 24, daenerys);
 const bronn = new Escudero("Bronn", Personaje.familia.blackwater, 36, jamie, 2);
 
 const personajes = [joffrey, jamie, daenerys, tyrion, bronn];
-console.log(personajes);
-
-const comunicados = personajes
-  .filter((personaje) => personaje instanceof Luchador)
-  .map((personaje) => personaje.comunicar());
-
-console.log(joffrey.serie);
-comunicados.forEach((mensaje) => console.log(mensaje));
-console.log(`Jaime vivo ? ${jamie.vivo}`);
-console.log(`Tyirion vivo ? ${tyrion.vivo}`);
-jamie.morir();
-tyrion.morir();
-console.log(`Jaime vivo ? ${jamie.vivo}`);
-console.log(`Tyirion vivo ? ${tyrion.vivo}`);
