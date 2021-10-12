9220307ae0f2bed64450de7cf098cee08f3e0908
Menchu MV <mcmvillalobos@gmail.com>
2021-10-12 17:48:17 +0200

    Imported other scripts into new file

---
+++ public/scripts/PersonajesCard.js
@@ -0,0 +1,6 @@
+import Asesor from "./Asesor.js";
+import Escudero from "./Escudero.js";
+import Luchador from "./Luchador.js";
+import Personaje from "./Personaje.js";
+import Rey from "./Rey.js";
+import personajes from "../index.js";
